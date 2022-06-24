77ef2eeca30ce0f4adde657ed7becb465c9b2fcf
Patryk Ruta <p4trykruta@gmail.com>
2022-06-24 22:36:10 +0200

    cosmetics

--- wp-content/themes/customTheme-child-theme/header.php
+++ wp-content/themes/customTheme-child-theme/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
+	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="https://gmpg.org/xfn/11">
@@ -9,7 +9,7 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 		<?php wp_head(); ?>
-  </head>
+	</head>
 
  	<body <?php body_class(); ?>>
 
@@ -27,7 +27,7 @@
 					<span class="line line1"></span>
 					<span class="line line2"></span>
 					<span class="line line3"></span>
-				</div> 
+				</div>
 				<div class="mainMenu">
 					<a href="#">o nas</a>
 					<a href="#">oferta</a>
